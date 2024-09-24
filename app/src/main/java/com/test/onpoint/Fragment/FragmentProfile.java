6e1a3db9d6767a952f8fae36d62bd437d09f3964
LinusArdell <linus2016chaesa@mhs.mdp.ac.id>
2024-09-24 15:07:17 +0700

    Update

--- app/src/main/java/com/test/onpoint/Fragment/FragmentProfile.java
+++ app/src/main/java/com/test/onpoint/Fragment/FragmentProfile.java
@@ -1,27 +1,30 @@
 package com.test.onpoint.Fragment;
 
-import android.app.Activity;
+import android.Manifest;
 import android.app.AlertDialog;
-import android.app.Dialog;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -49,6 +52,7 @@ public class FragmentProfile extends Fragment {
     private DatabaseReference databaseReference, userDatabase;
     private FirebaseAuth userAuth;
     LinearLayout llResetPassword, llLogout, llRegister, llDownloadExcel;
+    private static final int PERMISSION_STORAGE_CODE = 1000;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -253,6 +257,20 @@ public class FragmentProfile extends Fragment {
     }
 
     private void setDownloadExcel(){
+        String url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSlBkOtlUi_8ROqZxxeID4-pniUSl4s9plF5WVtonEEep3EJRBC1VOFvVhOnSngu8pCOaNQJaepMBdk/pub?output=xlsx";
 
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setTitle("File Spreadsheet");
+        request.setDescription("Mengunduh file spreadsheet...");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(getContext(), Environment.DIRECTORY_DOWNLOADS, "Check-Point.xlsx");
+
+        DownloadManager downloadManager = (DownloadManager) requireContext().getSystemService(Context.DOWNLOAD_SERVICE);
+        if (downloadManager != null) {
+            downloadManager.enqueue(request);
+            Toast.makeText(requireContext(), "Mengunduh file...", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(requireContext(), "Gagal mengakses DownloadManager", Toast.LENGTH_SHORT).show();
+        }
     }
 }
